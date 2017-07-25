7a20e7ad95ebf532974cb25cb4e8a3f80cfe6964
diane jones <dleighj1@msn.com>
2017-07-25 09:37:11 -0400

    New stuff

---
+++ app.js
@@ -0,0 +1,12 @@
+//Hands On Project Two: Bing Bong
+for (let i = 0; i <= 100; i++){
+  if (i){
+    if (i % 3 === 0)
+    window.alert(i + " Bing");
+    if (i % 5 === 0)
+    window.alert(i + " Bong");
+    if (i % 3 && 5 === 0)
+    window.alert( i + " Bing Bong");
+  }
+
+}
